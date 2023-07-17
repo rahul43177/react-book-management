4279a57b7d709cfabcd6c132c916c6714d815e19
Rahul <rahulmishra4317@gmail.com>
2023-07-18 03:44:39 +0530

    update json.db file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import {useState} from 'react'
+import {useState , useEffect} from 'react'
 import BookCreate from './components/BookCreate'
 import BookList from './components/BookList'
 import axios from 'axios'
 function App() {
     const [books , setBooks] = useState([]) 
 
+    const fetchBooks = async function() {
+        const response = await axios.get('http://localhost:3001/books')
+        setBooks(response.data)
+    } 
+
+    useEffect(()=>{
+        fetchBooks()
+    },[])
+    
 
     const editBookById = (id , newTitle) =>{
         const updateBooks = books.map((book)=>{
